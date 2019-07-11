1b8c65a2ed8b5428b881839d64bb3bc4028da011
gergopc <gergoesjanka@gmail.com>
2019-07-11 11:42:38 +0200

    Created documentation for ViewHolder

--- app/src/main/java/ro/bolyai/fivedice/gui/listview/ViewHolder.java
+++ app/src/main/java/ro/bolyai/fivedice/gui/listview/ViewHolder.java
@@ -3,22 +3,55 @@ package ro.bolyai.fivedice.gui.listview;
 
 import android.widget.TextView;
 
+/**
+ * Holds the Widget references of
+ * a single ListViewItem which is going to
+ * be generated in the ListViewAdapter.
+ * <p>
+ * It has no Logic, no generation of
+ * Objects/Widgets what so ever.
+ */
 public class ViewHolder {
 
     //region 1. Decl and Init
+
+    /**
+     * Will display the {@link ro.bolyai.fivedice.model.Player} Names in a single
+     * ListViewItem. Which is integerated / shown in
+     * the ListView Widget in the {@link ro.bolyai.fivedice.gui.LeaderboardActivity}
+     */
     private TextView txtPlayerName;
 
+    /**
+     * Will display the {@link ro.bolyai.fivedice.model.Player} PvP Wins
+     * in a single ListViewItem. Which is integrateed / shown in
+     * the ListView Widget in the {@link ro.bolyai.fivedice.gui.LeaderboardActivity}
+     */
     private TextView txtPlayerPvPWins;
 
+    /**
+     * Will display the {@link ro.bolyai.fivedice.model.Player} PvE Wins
+     * in a single ListViewItem. Which is integrateed / shown in
+     * the ListView Widget in the {@link ro.bolyai.fivedice.gui.LeaderboardActivity}
+     */
     private TextView txtPlayerPvEWins;
 
     //endregion
 
     //region 2. Constructor
-    public ViewHolder(TextView txtPlayerName, TextView txtPlayerPvPWins, TextView txtPlayerPvEWins){
-        this.txtPlayerName=txtPlayerName;
-        this.txtPlayerPvPWins =txtPlayerPvPWins;
-        this.txtPlayerPvEWins =txtPlayerPvEWins;
+
+    /**
+     * Default Constructor to set the Widget referenves from
+     * a single ListViewItem directly
+     *
+     * @param txtPlayerName : {@link TextView} : {@link ro.bolyai.fivedice.model.Player} name
+     * @param txtPlayerPvPWins : {@link TextView} : {@link ro.bolyai.fivedice.model.Player} PvP Wins
+     * @param txtPlayerPvEWins : {@link TextView} : {@link ro.bolyai.fivedice.model.Player} PvE Wins
+     */
+    public ViewHolder(TextView txtPlayerName, TextView txtPlayerPvPWins, TextView txtPlayerPvEWins) {
+        this.txtPlayerName = txtPlayerName;
+        this.txtPlayerPvPWins = txtPlayerPvPWins;
+        this.txtPlayerPvEWins = txtPlayerPvEWins;
     }
 
     //endregion
@@ -28,9 +61,11 @@ public class ViewHolder {
     public TextView getTxtPlayerName() {
         return txtPlayerName;
     }
+
     public TextView getTxtPlayerPvPWins() {
         return txtPlayerPvPWins;
     }
+
     public TextView getTxtPlayerPvEWins() {
         return txtPlayerPvEWins;
     }
@@ -38,9 +73,11 @@ public class ViewHolder {
     public void setTxtPlayerName(TextView txtPlayerName) {
         this.txtPlayerName = txtPlayerName;
     }
+
     public void setTxtPlayerPvPWins(TextView txtPlayerPvPWins) {
         this.txtPlayerPvPWins = txtPlayerPvPWins;
     }
+
     public void setTxtPlayerPvEWins(TextView txtPlayerPvEWins) {
         this.txtPlayerPvEWins = txtPlayerPvEWins;
     }
