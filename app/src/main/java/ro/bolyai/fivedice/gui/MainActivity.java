34f3b4da39b985e8b32558741316d3aab99db839
Teve <evelinturony2001@yahoo.com>
2019-07-11 09:44:38 +0200

    Created MainActivity. Designed its layout.

---
+++ app/src/main/java/ro/bolyai/fivedice/gui/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.bolyai.fivedice.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ro.bolyai.fivedice.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
