ba084e1922c92cbd8c14b7cfeed01ed815285b89
gergopc <gergoesjanka@gmail.com>
2019-07-11 10:06:07 +0200

    Initialized widgets in MainActivity java class. Created MainActivityListener

--- app/src/main/java/ro/bolyai/fivedice/gui/MainActivity.java
+++ app/src/main/java/ro/bolyai/fivedice/gui/MainActivity.java
@@ -2,14 +2,50 @@ package ro.bolyai.fivedice.gui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import ro.bolyai.fivedice.R;
+import ro.bolyai.fivedice.logic.MainActivityListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    //region 0. Constants
+    //endregion
+
+    //region 1. Decl. and Init widgets and attributes
+    ImageView imgFiveDice;
+
+    Button btnPlay;
+    Button btnLeaderboard;
+
+    MainActivityListener mainActivityListener;
+    //endregion
+
+    //region 2. LifeCircle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //1. Setting the Layout
         setContentView(R.layout.main_activity_layout);
+
+        //2. Generate the Widgets
+        imgFiveDice = findViewById(R.id.imgFiveDice);
+
+        btnPlay = findViewById(R.id.btnPlay);
+        btnLeaderboard = findViewById(R.id.btnLeaderboard);
+
+        //3. Generate the Listener
+        mainActivityListener = new MainActivityListener(this);
+
+        //4. Add Listener
+        btnPlay.setOnClickListener(mainActivityListener);
+        btnLeaderboard.setOnClickListener(mainActivityListener);
+
     }
+    //endregion
+
+    //region 3. Helper functions and methods
+    //endregion
 }
---
+++ app/src/main/java/ro/bolyai/fivedice/logic/MainActivityListener.java
@@ -0,0 +1,41 @@
+package ro.bolyai.fivedice.logic;
+
+import android.content.Context;
+import android.view.View;
+
+import ro.bolyai.fivedice.gui.MainActivity;
+
+public class MainActivityListener implements View.OnClickListener {
+
+    //region 0. Constants
+    //endregion
+
+    //region 1. Decl. and init Variables
+    private MainActivity mainActivity;
+
+    //endregion
+
+    //region 2. Constructor
+
+    public MainActivityListener(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    //endregion
+
+    //region 3. Getters and Setters
+    //endregion
+
+    //region 4. OnClick Handling Buttons and Views
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    //endregion
+
+    //region 5. Helperfunctions and methods
+    //endregion
+
+}
